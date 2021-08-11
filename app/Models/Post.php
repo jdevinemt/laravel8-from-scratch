8633f92324c6f374ca7e0eddf796c3f514111426
jdevine <joseph.devine.mt@gmail.com>
2021-08-10 19:46:47 -0600

    initial commit

---
+++ app/Models/Post.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+
+class Post {
+
+    public function __construct(public string $title, public string $excerpt, public int $date, public string $body, public string $slug){
+
+    }
+
+
+    public static function all(): Collection {
+
+        return collect(File::files(resource_path('posts')))
+            ->map(fn($file) => YamlFrontMatter::parseFile($file))
+            ->map(fn($document) => new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug
+            ));
+
+    }
+
+
+    public static function find(string $slug): ?self {
+
+        return static::all()->firstWhere('slug', '=', $slug);
+
+    }
+
+}
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+
+    <?php foreach($posts as $post) : ?>
+
+        <article>
+
+            <a href="/posts/<?= $post->slug; ?>">
+                <h1><?= $post->title; ?></h1>
+            </a>
+
+            <div>
+                <?= $post->excerpt; ?>
+            </div>
+
+        </article>
+
+    <?php endforeach; ?>
+
+</body>
