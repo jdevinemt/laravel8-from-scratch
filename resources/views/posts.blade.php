2c118dcafe9141404668719084b1dec2d498f7fb
jdevine <joseph.devine.mt@gmail.com>
2021-08-29 16:57:47 -0600

    episode 30

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,9 +4,13 @@
 
         <article>
 
-            <a href="/posts/{{$post->id}}">
+            <a href="/posts/{{$post->slug}}">
                 <h1>{{ $post->title }}</h1>
             </a>
+
+            <p>
+                By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
+            </p>
 
             <div>
                 {{ $post->excerpt }}
