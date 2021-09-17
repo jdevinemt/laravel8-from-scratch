45d8674b28661c84f2dbd0cf4b1da87f40686ee1
jdevine <joseph.devine.mt@gmail.com>
2021-09-16 20:10:47 -0600

    episode 36

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,10 @@ Route::get('/', function () {
 
     return view('posts', [
         'posts' => Post::latest()->with('category', 'author')->get(),
+        'categories' => Category::all(),
     ]);
 
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function(Post $post){
 
@@ -38,14 +39,17 @@ Route::get('categories/{category:slug}', function (Category $category) {
 
     return view('posts', [
         'posts' => $category->posts->load('category', 'author'),
+        'currentCategory' => $category,
+        'categories' => Category::all(),
     ]);
 
-});
+})->name('category');
 
 Route::get('authors/{author:username}', function (User $author) {
 
     return view('posts', [
         'posts' => $author->posts->load('category', 'author'),
+        'categories' => Category::all(),
     ]);
 
 });
