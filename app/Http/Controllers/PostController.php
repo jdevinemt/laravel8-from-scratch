2af7725ec8fd17ea5811b22e7a5ab9f127b01d54
jdevine <joseph.devine.mt@gmail.com>
2021-09-25 14:06:44 -0600

    episode 43

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,9 +11,8 @@ class PostController extends Controller
 
     public function index(){
 
-        return view('posts', [
-            'posts' => Post::latest()->filter(request(['search']))->get(),
-            'categories' => Category::all(),
+        return view('posts.index', [
+            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get(),
         ]);
 
     }
@@ -21,7 +20,7 @@ class PostController extends Controller
 
     public function show(Post $post){
 
-        return view('post', [
+        return view('posts.show', [
             'post'  => $post
         ]);
 
