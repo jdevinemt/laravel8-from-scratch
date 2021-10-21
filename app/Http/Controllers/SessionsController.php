fec194735e2d22eaabbec4ceed2e42b91d89a508
jdevine <joseph.devine.mt@gmail.com>
2021-10-20 19:35:59 -0600

    episode 49

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionsController extends Controller
+{
+
+    public function destroy(){
+
+        Auth::logout();
+
+        return redirect('/')->with('success', 'Goodbye!');
+
+    }
+
+}
