2992cbe3e8895a408c0aa2823460d813399d2eb6
jdevine <joseph.devine.mt@gmail.com>
2021-10-20 18:46:41 -0600

    episode 46

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,6 +32,10 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    public function setPasswordAttribute(string $password){
+        $this->attributes['password'] = bcrypt($password);
+    }
+
     public function posts(){
         return $this->hasMany(Post::class);
     }
