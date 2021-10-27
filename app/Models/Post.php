cfd38eaf4da0e208bb1358582583973eff3af486
jdevine <joseph.devine.mt@gmail.com>
2021-10-26 20:10:48 -0600

    episode 54

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -47,4 +47,8 @@ class Post extends Model
         return $this->belongsTo(User::class, 'user_id');
     }
 
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
+
 }
