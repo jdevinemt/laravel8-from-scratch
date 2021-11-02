8bb8ff13ba1a6e2861f346ab6fe9cdd5853b01a4
jdevine <joseph.devine.mt@gmail.com>
2021-11-01 19:23:52 -0600

    episode 55

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -50,9 +50,27 @@
             </div>
 
             <section class="col-span-8 col-start-5 mt-10 space-y-6">
+
+                <x-panel>
+                    <form method="POST" action="" >
+                        @csrf
+                        <header class="flex items-center">
+                            <img src="https://i.pravatar.cc/40?u={{auth()->id()}}" alt="" class="rounded-full">
+                            <h2 class="ml-3">Want to participate?</h2>
+                        </header>
+                        <div class="mt-6">
+                            <textarea name="" class="w-full text-sm focus:outline-none focus:ring" rows="5" placeholder="Quick, think of something to say."></textarea>
+                        </div>
+                        <div class="flex justify-end mt-5 pt-5 border-t border-gray-200">
+                            <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
+                        </div>
+                    </form>
+                </x-panel>
+
                 @foreach ($post->comments as $comment)
                     <x-post-comment :comment="$comment"></x-post-comment>
                 @endforeach
+
             </section>
 
         </article>
