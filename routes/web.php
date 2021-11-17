2275ec7c10e3622140ec0f883fee91ba0ae0e82b
jdevine <joseph.devine.mt@gmail.com>
2021-11-16 18:12:36 -0700

    episode 60

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\NewsletterController;
 use App\Http\Controllers\PostCommentController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionsController;
+use App\Services\Newsletter;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\ValidationException;
 
@@ -18,38 +20,6 @@ use Illuminate\Validation\ValidationException;
 |
 */
 
-Route::post('newsletter', function(){
-
-    request()->validate([
-        'email' => ['required', 'email']]
-    );
-
-    $mailchimp = new \MailchimpMarketing\ApiClient();
-
-    $mailchimp->setConfig([
-        'apiKey' => config('services.mailchimp.key'),
-        'server' => 'us20'
-    ]);
-
-    try{
-
-        $response = $mailchimp->lists->addListMember('1937513b15', [
-            'email_address' => request('email'),
-            'status' => 'subscribed',
-        ]);
-
-    }catch(\Exception $e){
-
-        throw ValidationException::withMessages([
-            'email' => 'This email could not be added to our newsletter list.',
-        ]);
-
-    }
-
-    return redirect('/')->with('You are now signed up for our newsletter!');
-
-});
-
 Route::get('/', [PostController::class, 'index'])->name('home');
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::post('posts/{post:slug}/comments', [PostCommentController::class, 'store']);
@@ -60,3 +30,5 @@ Route::post('register', [RegisterController::class, 'store'])->middleware('guest
 Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
 Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
 Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
+
+Route::post('newsletter', NewsletterController::class);
